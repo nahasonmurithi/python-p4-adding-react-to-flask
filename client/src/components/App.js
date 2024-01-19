41757093f944ac6bc7373d729db40463d3cebdf0
Nahason <nahason.murithi@student.moringaschool.com>
2024-01-19 23:57:09 +0300

    completed

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -11,6 +11,17 @@ function App() {
   const [messages, setMessages] = useState([]);
   const [search, setSearch] = useState("");
 
+  // useEffect(() => {
+  //   fetch("http://127.0.0.1:5555/messages")
+  //     .then(r => {
+  //       if (r.ok) {
+  //         return r.json
+  //       }
+  //       throw r;
+  //     })
+  //     .then((messages) => setMessages(messages))
+  // }, []);
+
   useEffect(() => {
     fetch("http://127.0.0.1:5555/messages")
       .then((r) => r.json())
